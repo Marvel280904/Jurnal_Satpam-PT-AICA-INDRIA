9905ac350ac0d59d6b8cc75d4f10ca0daef5513f
umam29 <qomarul.u29@gmail.com>
2025-11-05 10:35:05 +0700

    menambahkan logo pada title tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title', 'PT AICA INDRIA')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="{{ url('images/logo-lem-fox-bg.jpeg') }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @stack('styles')
 </head>
